b5959d7146b102fd4eda7e347fdf7035c4e82586
fedejinich <federico@rootstocklabs.com>
2024-12-17 11:57:55 -0300

    code cleanup

--- pkmn_guess/build.sh
+++ pkmn_guess/build.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
 
-# Exit the script if any command fails
+# exit the script if any command fails
 set -e
 
 NAME="pkmn_guess"
 
-# Define paths
 BUILD_DIR="zig-out"
 OUTPUT_FILE=$BUILD_DIR"/bin/"$NAME
 BITVMX_CPU_DIR="../bitvmx_protocol/BitVMX-CPU"
 ELF_FILE_NAME=$NAME".elf"
+INPUT=00001234
 
-# Clean previous build
 echo "Cleaning previous build..."
 rm -rf $BUILD_DIR
 
-# Build the project
 echo "Building project..."
 zig build
 
-# Copy the ELF file to the required directories
+# echo "Cleaning previous elfs..."
+# rm $BITVMX_CPU_DIR/$ELF_FILE_NAME
+# rm $BITVMX_CPU_DIR/docker-riscv32/riscv32/build/$ELF_FILE_NAME
+# rm "../bitvmx_protocol/execution_files/"$ELF_FILE_NAME
+# rm $BITVMX_CPU_DIR/checkpoint.*
+
 echo "Copying $ELF_FILE_NAME to target directories..."
 cp $OUTPUT_FILE $BITVMX_CPU_DIR/$ELF_FILE_NAME
+cp $OUTPUT_FILE $BITVMX_CPU_DIR/docker-riscv32/riscv32/build/$ELF_FILE_NAME
 cp $OUTPUT_FILE "../bitvmx_protocol/execution_files/"$ELF_FILE_NAME
 
-# Change to the BitVMX-CPU directory
 cd $BITVMX_CPU_DIR
 
-# Run the program
 echo "Running the program..."
-cargo run --release --bin emulator -- execute --elf $ELF_FILE_NAME --debug --checkpoints --input 00001234deadbeef --input-as-little
+cargo run --release --bin emulator -- execute --elf $ELF_FILE_NAME --debug --checkpoints --input $INPUT --input-as-little
 
-# Generate ROM commitment
 echo "Generating ROM commitment..."
 cargo run --release --bin emulator -- generate-rom-commitment --elf $ELF_FILE_NAME --sections > ../execution_files/pkmn_rom_commitment.txt
 
-# Generate instruction mapping
 echo "Generating instruction mapping..."
 cargo run --release --bin emulator -- instruction-mapping > ../execution_files/instruction_mapping.txt
+
+echo "Generating execution trace..."
+cargo run --release --bin emulator -- execute --elf $ELF_FILE_NAME --input $INPUT --trace > ../verifier_files/execution_trace.txt
+cargo run --release --bin emulator -- execute --elf $ELF_FILE_NAME --input $INPUT --trace > ../prover_files/execution_trace.txt
 
 echo "Build completed successfully!"
 
--- pkmn_guess/entrypoint.s
+++ pkmn_guess/entrypoint.s
@@ -1,23 +1,13 @@
 .section .text._start;
 .globl _start;
 _start:
-
 	li a0, 0	# set 0 as calling argument
 	call main
 	j _halt
 
 _halt:
-
-
 	li a7, 93
 	ecall
-
-
-# data section
-.section .data
-my_data:
-    .word 0xDEADBEEF
-
 
 .section .stack, "aw", @nobits
 .globl _stack_start
