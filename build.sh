12687930d3d11468e3b667f0ad7753ddd08ae491
fedejinich <federico@rootstocklabs.com>
2025-03-17 13:59:29 -0300

    using test_input

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-INPUT=00001234 # set the input here
-
 # exit the script if any command fails
 set -e
+
+INPUT=00001234 # set the input here
 
 NAME="pkmn_bttl"
 
@@ -27,21 +27,18 @@ zig build
 # rm "../bitvmx_protocol/execution_files/"$ELF_FILE_NAME
 # rm $BITVMX_CPU_DIR/checkpoint.*
 
-echo "Copying $ELF_FILE_NAME to target directories..."
+echo "Copying $ELF_FILE_NAME to CPU directories..."
 cp $OUTPUT_FILE $BITVMX_CPU_DIR/$ELF_FILE_NAME
 cp $OUTPUT_FILE $BITVMX_CPU_DIR/docker-riscv32/riscv32/build/$ELF_FILE_NAME
 mkdir -p "../bitvmx_protocol/execution_files" && cp -p $OUTPUT_FILE "../bitvmx_protocol/execution_files/"$ELF_FILE_NAME
-
-cd $BITVMX_CPU_DIR
 
 echo "Running the program..."
 cargo run --release --bin emulator -- execute --elf $ELF_FILE_NAME --debug --checkpoints --input $INPUT --input-as-little
 
 echo "Generating ROM commitment..."
-cargo run --release --bin emulator -- generate-rom-commitment --elf $ELF_FILE_NAME --sections > ../execution_files/pkmn_rom_commitment.txt
+cargo run --release --bin emulator -- generate-rom-commitment --elf $ELF_FILE_NAME --sections >../execution_files/pkmn_rom_commitment.txt
 
 echo "Generating instruction mapping..."
-cargo run --release --bin emulator -- instruction-mapping > ../execution_files/instruction_mapping.txt
+cargo run --release --bin emulator -- instruction-mapping >../execution_files/instruction_mapping.txt
 
 echo "Build completed successfully!"
-
