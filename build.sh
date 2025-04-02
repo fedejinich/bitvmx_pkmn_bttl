bb73a4bc72c1965b4e5fff8b6d0ababc9ad4e2fa
fede <federico@rootstocklabs.com>
2025-04-02 14:00:53 -0300

    fix build.sh with manifest path

--- build.sh
+++ build.sh
@@ -21,24 +21,18 @@ rm -rf $BUILD_DIR
 echo "Building project..."
 zig build
 
-# echo "Cleaning previous elfs..."
-# rm $BITVMX_CPU_DIR/$ELF_FILE_NAME
-# rm $BITVMX_CPU_DIR/docker-riscv32/riscv32/build/$ELF_FILE_NAME
-# rm "../bitvmx_protocol/execution_files/"$ELF_FILE_NAME
-# rm $BITVMX_CPU_DIR/checkpoint.*
-
 echo "Copying $ELF_FILE_NAME to CPU directories..."
 cp $OUTPUT_FILE $BITVMX_CPU_DIR/$ELF_FILE_NAME
 cp $OUTPUT_FILE $BITVMX_CPU_DIR/docker-riscv32/riscv32/build/$ELF_FILE_NAME
 mkdir -p "../bitvmx_protocol/execution_files" && cp -p $OUTPUT_FILE "../bitvmx_protocol/execution_files/"$ELF_FILE_NAME
 
 echo "Running the program..."
-cargo run --release --bin emulator -- execute --elf $ELF_FILE_NAME --debug --checkpoints --input $INPUT --input-as-little
+cargo run --manifest-path ../bitvmx_protocol/BitVMX-CPU/Cargo.toml --release --bin emulator -- execute --elf $ELF_FILE_NAME --debug --checkpoints --input $INPUT --input-as-little
 
 echo "Generating ROM commitment..."
-cargo run --release --bin emulator -- generate-rom-commitment --elf $ELF_FILE_NAME --sections >../execution_files/pkmn_rom_commitment.txt
+cargo run --manifest-path ../bitvmx_protocol/BitVMX-CPU/Cargo.toml --release --bin emulator -- generate-rom-commitment --elf $ELF_FILE_NAME --sections >../bitvmx_protocol/execution_files/pkmn_rom_commitment.txt
 
 echo "Generating instruction mapping..."
-cargo run --release --bin emulator -- instruction-mapping >../execution_files/instruction_mapping.txt
+cargo run --manifest-path ../bitvmx_protocol/BitVMX-CPU/Cargo.toml --release --bin emulator -- instruction-mapping >../bitvmx_protocol/execution_files/instruction_mapping.txt
 
 echo "Build completed successfully!"
---
+++ todo.md
@@ -0,0 +1,4 @@
+- agregar el mainfest path a los comandos de cargo de build.sh
+- explicitar que se corrio con zig 13.0
+  - actualizar a zig 14?
+- invsetigar el tema de los paths relativos en los comandos de running (por que los tuve que modificar?)
